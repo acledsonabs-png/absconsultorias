2e9c0bde7c27cb273d86036ccc4ef04ae1b39b83
acledsonabs-png <acledsonabs@gmail.com>
2026-06-29 12:26:55 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
+export const supabase = createClient(
+  "https://epcnujsdoxnwkbeemxyv.supabase.co",
+  "COLE_SUA_ANON_KEY_AQUI"
+);
